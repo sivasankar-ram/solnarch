8229f22d158a9bfe0d8b19c47336cc8e2f91c6f3
Sivasankar Ramalingam <siva@kloudtom.com>
2023-12-16 11:20:39 +0000

    Polls APP removed from main branch

--- solnarch/solnarch/settings.py
+++ solnarch/solnarch/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
